13b6ae346877a0e34c9b533d3eb555178c071cdc
Deependra Singh <ds@menteconsulting.com>
2024-11-20 17:51:03 +0530

    Updated Test build

--- src/test/java/com/boldnest/em_main/EmployeeManagementApplicationTests.java
+++ src/test/java/com/boldnest/em_main/EmployeeManagementApplicationTests.java
@@ -3,11 +3,13 @@ package com.boldnest.em_main;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.boldnest.em.EmployeeManagementApplication;
+
+@SpringBootTest(classes = EmployeeManagementApplication.class)
 class EmployeeManagementApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        // This test ensures that the application context loads successfully
+    }
 }
