9ae27a9b3210d0727c1abd7a723a35c652f2e43e
Deependra Singh <ds@menteconsulting.com>
2024-11-20 17:42:07 +0530

    Updated

--- src/main/java/com/boldnest/em/EmployeeManagementApplication.java
+++ src/main/java/com/boldnest/em/EmployeeManagementApplication.java
@@ -2,14 +2,12 @@ package com.boldnest.em;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = "com.boldnest.em")
-@EnableJpaRepositories(basePackages = "com.boldnest.em.repository")
+@SpringBootApplication
 public class EmployeeManagementApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmployeeManagementApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeManagementApplication.class, args);
+    }
 }
+
